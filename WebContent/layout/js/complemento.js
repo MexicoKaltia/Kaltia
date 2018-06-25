03f97277597733ff955abd97e608e5c4cecda0da
Hugo G RIVAS <hugo.rivas@vun.mx>
2018-06-25 18:25:56 -0500

    250618

--- WebContent/layout/js/complemento.js
+++ WebContent/layout/js/complemento.js
@@ -47,23 +47,21 @@ $(document).ready(function() {
 //		       return decodeURI(results[1]) || 0;
 //		    }
 //	}
-	
+	$.paramInicial = "AAA";
+	$.variableParam = "tipo";
 	$.param ="";
-	$.dataSeccion ="ss";
-	
+	$.respuestaSeccion ="";
 	$.urlParam = $(function(){
-		var tipo = "tipo";
-		var results = new RegExp('[\?&]' + tipo + '=([^&#]*)').exec(window.location.href);
-		
+		var results = new RegExp('[\?&]' + $.variableParam + '=([^&#]*)').exec(window.location.href);
 		if (results == null){
 			_desactiveSeccionEdicion();
 			console.log("results null")
 		       return null;
 		    }else{
-		    	if(decodeURI(results[1]) == "AAA"){
+		    	if(decodeURI(results[1]) === $.paramInicial){
 		    		$('div.headerSeccion1').addClass("edicion")
 		    		$('div.headerSeccion2').addClass("edicion")
-		    	console.log("PARAM1:"+decodeURI(results[1]));
+		    	//console.log("PARAM1:"+decodeURI(results[1]));
 		    	$.param = decodeURI(results[1]);
 		       //return decodeURI(results[1]) || 0;
 		    	return $.param;
@@ -79,45 +77,77 @@ $(document).ready(function() {
 		$('div').removeClass("edicion headerSeccion1 headerSeccion2 headerSeccion3 headerSeccion4")
 		$('nav').removeClass("edicion")
 	}
+	   
 	
-	function _getDataSeccion(action, section){
-		console.log(action+":"+seccion);
-		$.getJSON("http://rest-service.guides.spring.io/greeting", "", function (data, status) {
-			  if (status === 200) {
-				  console.log(data.id);
-			      console.log(data.content);
-				  data = ["acerca.refAcerca", "contacto.refContacto", "registro.refRegistro", "ingresa.refIngresa"];
-				  return data;
-			  }
-			});
-	    	
-		
-		
+	function seccionValoresLectura (action, section, callback) {
+		  var scriptURL = "somefile.php?name=" + action + section;
+		  return $.getJSON("http://rest-service.guides.spring.io/greeting", callback);
+	}
+
 	
+	//METODO SINCRONO PARA INVOCAR AL SECCION LECTURA
+	function _getDataSeccion(action, section, jsonLectura){
+		console.log(action+":1:"+seccion);
+		console.log($.param);
+		parametroCliente = {"action":action, "section":section};
+//		$.getJSON("http://rest-service.guides.spring.io/greeting", resultData, async: false);
+		$.ajax({
+			dataType: "json",
+			url: "http://rest-service.guides.spring.io/greeting",
+			type: "GET",
+			async: false,
+			success: resultData
+		})
+	}
+	
+	function resultData(respuesta){
+		console.log(respuesta);		
+		$.respuestaSeccion = respuesta;
+		return $.respuestaSeccion;
 	}
 	
 	
 	$('.headerSeccion1').click(function(){
-		if($.param === "AAA"){
-			console.log("param:"+$.param)
-			
+		if($.param === $.paramInicial){
+			//console.log("param:"+$.param)
+			console.log(window.location.href);
 			action = "action";
 			seccion = "seccion";
-	        var data = _getDataSeccion(action, seccion)
-			headerSeccion1Json = {"arrayText" :"text"}
-				
-			$(this).attr("data-toggle","modal");
-			$(this).attr("data-target","#headerSeccion1");
-			console.log(data)
-			$(data).each(function(i){
-				console.log(data[i])
-				valor ="<div class='input-group'><div class='input-group-prepend'><input class='form-control' aria-label='Text input with checkbox' type='text' placeholder="+data[i]+"><div class='input-group-text'><input aria-label='Checkbox for following text input' type='checkbox' id="+$(i).text()+"_chk></div></div></div>";
+			jsonLectura = {"arrayText" :"text"}
+			 
+			$('.headerSeccion1').attr("data-toggle","modal");
+			$('.headerSeccion1').attr("data-target","#headerSeccion1");
+			$("div.soloLectura > div").remove();
+			//_getDataSeccion(action, seccion, jsonLectura)    SINCRONO	 
+		seccionValoresLectura(action, seccion, function(data){  //     ASYNCRONO
+			$.respuestaSeccion = data;
+			console.log($.respuestaSeccion)
+			 $.respuestaSeccion = {"arrayText":["ACERCA DE NOSOTROS.referencia1",  "CONTACTO.referencia2", "REGISTRO.referencia3", "INGRESA.referencia4"]};
+			//console.log($.respuestaSeccion)
+			$($.respuestaSeccion.arrayText).each(function(i){
+				console.log($.respuestaSeccion.arrayText[i])
+				valor ="<div class='input-group'><div class='input-group-prepend'><input class='form-control' aria-label='Text input with checkbox' type='text' value="+$.respuestaSeccion.arrayText[i]+"><div class='input-group-text'><input aria-label='Checkbox for following text input' type='checkbox' id="+$(i).text()+"_chk></div></div></div>";
 				 $(valor).insertAfter($('.soloLectura_in'));
 				});
-			
-			
-			
-				
+	   });
+		
+		$('#agregarStr').click(function(){
+			var  valor2 =null;
+			valor2 = $('#headerSeccion1_ingresarStr').val();
+			if($('#headerSeccion1_ingresarStr').val() !== ""){
+//			 var valor3 ="<div class='input-group' id='"+$('#headerSeccion1_ingresarStr').val()+"'><div class='input-group-prepend id='"+$('#headerSeccion1_ingresarStr').val()+"''><input class='form-control' aria-label='Text input with checkbox' type='text' placeholder="+$('#headerSeccion1_ingresarStr').val()+" readonly><div class='input-group-text'><input aria-label='Checkbox for following text input' type='checkbox' id="+$('#headerSeccion1_ingresarStr').val()+"_chk></div></div></div>"
+			var valor3 ="<div class='input-group'><div class='input-group-prepend'><input class='form-control' aria-label='Text input with checkbox' type='text' value="+$('#headerSeccion1_ingresarStr').val()+"><div class='input-group-text'><input aria-label='Checkbox for following text input' type='checkbox' id="+$('#headerSeccion1_ingresarStr').val()+"_chk></div></div></div>";
+				$(valor3).insertAfter($('.soloLectura_in'));
+				$('#headerSeccion1_ingresarStr').val("");
+			}
+		})
+		
+		$('#headerSeccion1_btnClose').click(function(){
+			alert("Sin Guardar cambios");
+			$("div.soloLectura > div").remove();
+			console.log($("div.soloLectura").html());
+		});
+		
 		}else{
 			console.log("param:Nulo");
 		}
@@ -129,6 +159,8 @@ $(document).ready(function() {
 		if($.param != null){
 			console.log("param:"+$.param)
 			headerSeccion2Json = {"telefono" : "text","email" : "text"}
+			
+			
 			
 		}else{
 			console.log("param:Nulo");
